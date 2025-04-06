4e93ae52c287d4fdfc8a328e15f53341155dfeef
ApsaraWitharana <asachini095@gmail.com>
2025-04-06 16:06:58 +0530

    initial commit

--- backend/task-service/src/test/java/lk/ijse/gdse68/taskservice/TaskServiceApplicationTests.java
+++ backend/task-service/src/test/java/lk/ijse/gdse68/taskservice/TaskServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package lk.ijse.gdse68.backend;
+package lk.ijse.gdse68.taskservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class TaskServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
