3c16d91c12da13d973f9554b6a00328d476478c7
ApsaraWitharana <asachini095@gmail.com>
2025-04-08 21:19:15 +0530

    add-user login component impl

---
+++ frontend/src/app/auth/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+this.router.navigate(['/signup']);
+  }
+}
