498ad751ad7c5830068deb318357d79d69f82b80
ApsaraWitharana <asachini095@gmail.com>
2025-04-10 00:07:29 +0530

    add-user role base auth update

--- backend/user-service/src/main/java/lk/ijse/gdse68/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/lk/ijse/gdse68/userservice/UserServiceApplication.java
@@ -15,11 +15,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  #**/
 
 @CrossOrigin("*")
-@MultipartConfig(
-        fileSizeThreshold = 1024 * 1024 * 2, //2MB //upload krn file eke -ekathuwa file wal 1MB nam eke file ek process krn use krnne ram ek ek 2 t adu unam primary memory eke //4MB unot ek use krnne  ek file system ek//tiyen mb gane ekk awot ek ram ekt ynw nattm threshold ekk dal  seconder memary ekt ynw
-        maxFileSize = 1024 * 1024 * 10, // upload krn allow krn maximum file eke size eke //ex-10MB
-        maxRequestSize = 1024 * 1024 * 50 // upload file ekth ekk sampurna data wal ekthuwa //50MB
-)
 @SpringBootApplication
 public class UserServiceApplication {
 
